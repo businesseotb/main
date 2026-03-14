4bde1990699eb1f666b08300a5c1775c21365550
businesseotb <businesseotb@gmail.com>
2026-03-14 18:38:49 +0100

    Update release.js

--- js/release.js
+++ js/release.js
@@ -2,23 +2,23 @@
 Parse.initialize("AZ2atrslozmQ8GUb7iNfjuRQfpLI5WffQ4w8NCka", "mL8fa0LBZssoy82vPwQmtvW2Tz7IdpZBj9PfMASb");
 Parse.serverURL = "https://parseapi.back4app.com/";
 
-// Reference to gallery container
-const galleryGrid = document.getElementById("gallery-grid");
+// Reference to release container
+const releaseGrid = document.getElementById("release-grid");
 
 // Query release table
 const release = Parse.Object.extend("Z_PUB");
 const query = new Parse.Query(release);
 
 query.find().then((results) => {
-  galleryGrid.innerHTML = ""; // clear loading text
+  releaseGrid.innerHTML = ""; // clear loading text
 
-results.forEach((gallery) => {
-  const name = gallery.get("NAME");
-  const imageFile = gallery.get("IMAGE"); // this is a Parse.File
+results.forEach((release) => {
+  const name = release.get("NAME");
+  const imageFile = release.get("IMAGE"); // this is a Parse.File
   const imageUrl = imageFile ? imageFile.url() : ""; // get the actual URL
 
   const square = document.createElement("div");
-  square.className = "gallery-square";
+  square.className = "release-square";
 
   const img = document.createElement("img");
   img.src = imageUrl;
@@ -30,13 +30,13 @@ results.forEach((gallery) => {
   square.appendChild(img);
   square.appendChild(overlay);
   square.addEventListener("click", () => {
-    const releaseId = gallery.id;
+    const releaseId = release.id;
     window.location.href = `showreleas.html?release=${releaseId}`;
   });
-  galleryGrid.appendChild(square);
+  releaseGrid.appendChild(square);
 
   
 });
 }).catch((error) => {
-  galleryGrid.innerHTML = "Failed to load galleries: " + error.message;
+  releaseGrid.innerHTML = "Failed to load galleries: " + error.message;
 });
