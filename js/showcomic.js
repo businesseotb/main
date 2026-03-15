9a57ff776ff9d749edfeeb8cd14b860080cee4d2
businesseotb <businesseotb@gmail.com>
2026-03-15 15:38:43 +0100

    Update showcomic.js

--- js/showcomic.js
+++ js/showcomic.js
@@ -1,9 +1,5 @@
-console.log("showcomic.js loaded");
-
 Parse.initialize("AZ2atrslozmQ8GUb7iNfjuRQfpLI5WffQ4w8NCka", "mL8fa0LBZssoy82vPwQmtvW2Tz7IdpZBj9PfMASb");
 Parse.serverURL = "https://parseapi.back4app.com/";
-
-console.log("Parse initialized");
 
 const urlParams = new URLSearchParams(window.location.search);
 const comicId = urlParams.get("comic");
@@ -13,22 +9,18 @@ console.log("comicId:", comicId);
 const ImagesInComic = Parse.Object.extend("Z_IMAGES_IN_COMIC");
 const query = new Parse.Query(ImagesInComic);
 
-console.log("Query created");
-
 const comicPointer = new Parse.Object("Z_COMICS");
 comicPointer.id = comicId;
-
-console.log("Pointer created");
 
 query.equalTo("COMIC", comicPointer);
 query.include("IMAGE");
 
-console.log("Query configured");
+// sort pages
+query.ascending("PAGE");
 
 query.find()
 .then(results => {
 
-console.log("Results:", results);
 
 const grid = document.getElementById("comics-grid");
 
