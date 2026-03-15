166572ff58cfb080d70e9ad2d3d8a9352e7d8055
businesseotb <businesseotb@gmail.com>
2026-03-15 15:21:09 +0100

    Update showcomic.js

--- js/showcomic.js
+++ js/showcomic.js
@@ -1,65 +1,32 @@
+console.log("showcomic.js loaded");
+
 Parse.initialize("AZ2atrslozmQ8GUb7iNfjuRQfpLI5WffQ4w8NCka", "mL8fa0LBZssoy82vPwQmtvW2Tz7IdpZBj9PfMASb");
 Parse.serverURL = "https://parseapi.back4app.com/";
 
-console.log("showcomic.js loaded");
+console.log("Parse initialized");
 
 const urlParams = new URLSearchParams(window.location.search);
 const comicId = urlParams.get("comic");
 
-const ImagesInComic= Parse.Object.extend("Z_IMAGES_IN_COMIC");
+console.log("comicId:", comicId);
+
+const ImagesInComic = Parse.Object.extend("Z_IMAGES_IN_COMIC");
 const query = new Parse.Query(ImagesInComic);
+
+console.log("Query created");
 
 const comicPointer = new Parse.Object("Z_COMIC");
 comicPointer.id = comicId;
 
+console.log("Pointer created");
+
 query.equalTo("COMIC", comicPointer);
 query.include("IMAGE");
 
-query.find()
-.then(results => {
+console.log("Query configured");
 
-console.log("results:", results);
+query.find().then(results => {
 
-const grid = document.getElementById("comics-grid");
-
-results.forEach(item => {
-
-const imageObj = item.get("IMAGE");
-
-console.log("imageObj:", imageObj);
-
-const imageFile = imageObj.get("IMAGE");
-
-console.log("imageFile:", imageFile);
-
-const imageUrl = imageFile.url();
-
-const card = document.createElement("div");
-card.className = "image-card";
-
-card.innerHTML = `<img src="${imageUrl}">`;
-
-grid.appendChild(card);
+console.log("Results:", results);
 
 });
-
-})
-.catch(error => {
-console.error("Query error:", error);
-});
-
-const popup = document.getElementById("image-popup");
-const popupImg = document.getElementById("popup-img");
-
-function openPopup(url){
-
-popupImg.src = url;
-popup.classList.add("active");
-
-}
-
-popup.addEventListener("click", () => {
-popup.classList.remove("active");
-});
-
-document.addEventListener("contextmenu", e => e.preventDefault());
