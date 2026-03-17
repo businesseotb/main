1fbebf8600fde253361373363c9d0631d2fc2ead
businesseotb <businesseotb@gmail.com>
2026-03-17 12:13:32 +0100

    Update release.js

--- js/release.js
+++ js/release.js
@@ -3,7 +3,7 @@ Parse.initialize("AZ2atrslozmQ8GUb7iNfjuRQfpLI5WffQ4w8NCka", "mL8fa0LBZssoy82vPw
 Parse.serverURL = "https://parseapi.back4app.com/";
 
 // Reference to release container
-const releaseGrid = document.getElementById("galley-grid");
+const releaseGrid = document.getElementById("gallery-grid");
 
 // Query release table
 const release = Parse.Object.extend("Z_PUB");
